980c39203806b193ef0bf7af5b92abf73f74fb4b
bit-ksw <bit7521@gmail.com>
2019-05-09 22:02:43 +0900

    2-5.웹팩으로 빌드하기

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # react-webgame
+기본적인 바벨 최신문법으로 바꿔줌
+"@babel/core": "^7.4.4",
+
+환경에 맞게 바꿔주는거
+"@babel/preset-env": "^7.4.4",
+
+"@babel/preset-react": "^7.0.0",
+
+// 바벨이랑 웹팩 연결해줌
+"babel-loader": "^8.0.5",
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -5,13 +5,27 @@ module.exports = {
   mode: 'development', //실서비스: production
   devtool: 'eval',
   resolve: {
-    extensions: ['.js', 'jsx']
+    extensions: ['.js', '.jsx']
   },
 
   // 입력
   entry: {
     app: ['./client'],
   },
+
+  module: {
+    rules: [{
+      // 규칙을 적용할 파일들
+      test: /\.jsx?/,
+      // 어떤룰? 바벨룰~
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    }],
+  },
+
   // 출력
   output: {
     path: path.join(__dirname, 'dist'),
